5a4591616cce009ed553c3ef5b87753a44c25f5e
jaymes15 <jamesoluwatomisinomotosho@gmail.com>
2024-08-14 12:02:28 +0100

    first commit

--- models/user.py
+++ models/user.py
@@ -1,9 +1,14 @@
 from db import db
 
-
 class UserModel(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return f"<UserModel(id={self.id}, username='{self.username}')>"
+
+    def __str__(self):
+        return f"User(username='{self.username}', id={self.id})"
